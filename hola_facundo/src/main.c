a4a8f3c0e5959ab72a8efd965dbc057434575cf8
Facuu13 <facundoo30@gmail.com>
2026-02-03 11:37:04 -0300

    blink

--- hola_facundo/src/main.c
+++ hola_facundo/src/main.c
@@ -1,16 +1,37 @@
-
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
+
+/* Tomamos el LED desde el alias 'led0' que definimos en el overlay */
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "No se encontró el alias led0 en el DeviceTree. Revisa el overlay."
+#endif
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
-    printk("Hola Facundo! Zephyr corriendo en ESP32-C3 😄\n");
+    if (!gpio_is_ready_dt(&led)) {
+        printk("GPIO no listo para LED\n");
+        return 0;
+    }
+
+    int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+    if (ret < 0) {
+        printk("Error configurando LED: %d\n", ret);
+        return 0;
+    }
+
+    printk("Blinky OK: parpadeando LED desde DeviceTree\n");
 
     while (1) {
-        printk("tick: %lld ms\n", k_uptime_get());
-        k_sleep(K_SECONDS(1));
+        gpio_pin_toggle_dt(&led);
+        k_sleep(K_MSEC(500));
     }
 
     return 0;
 }
-
